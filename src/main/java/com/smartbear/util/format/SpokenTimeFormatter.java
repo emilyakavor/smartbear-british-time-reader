967e7e41301777b8099eceb86b794707f6965e99
Emil Yakavor <emil.yakavor@gmail.com>
2025-08-27 22:57:41 +0000

    Captured springboot version in pom

--- src/main/java/com/smartbear/util/format/SpokenTimeFormatter.java
+++ src/main/java/com/smartbear/util/format/SpokenTimeFormatter.java
@@ -1,6 +1,6 @@
-package com.smartbear.smartbearemil.core.format;
+package com.smartbear.util.format;
 
-import com.smartbear.smartbearemil.core.ClockTime;
+import com.smartbear.util.ClockTime;
 
 public interface SpokenTimeFormatter {
     String speak(ClockTime time);
